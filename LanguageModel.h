447233e8ccd628773d2e2e23af96ad8b8ce432c0
Kai Sheng Tai <kaishengtai@gmail.com>
2014-11-28 01:14:38 -0500

    Implement language model learning

--- LanguageModel.h
+++ LanguageModel.h
@@ -1,6 +1,7 @@
 #ifndef LANGUAGEMODEL_H
 #define LANGUAGEMODEL_H
 
+#include <unordered_map>
 #include <Eigen/Dense>
 
 #include "gen-cpp/distrust_types.h"
@@ -9,9 +10,10 @@ using Eigen::Dynamic;
 using Eigen::RowMajor;
 using Eigen::Matrix;
 using Eigen::Map;
+using Eigen::VectorXd;
 
-typedef Map<Matrix<double, Dynamic, Dynamic, RowMajor>> Matrix_t;
-typedef Map<Matrix<double, Dynamic, 1>> Vector_t;
+typedef Matrix<double, Dynamic, Dynamic, RowMajor> Matrix_t;
+typedef Matrix<double, Dynamic, 1> Vector_t;
 
 class LanguageModel {
  public:
@@ -24,8 +26,9 @@ class LanguageModel {
   void get_params(distrust::Params &ret);
   std::vector<double> forward(const std::vector<uint32_t> &input);
   //Eigen::MatrixXd forward(Eigen::MatrixXd input);
-  void backward(distrust::Params &ret, const std::vector<uint32_t> &input, const uint32_t target);
+  void backward(const std::vector<uint32_t> &input, const uint32_t target);
   //distrust::Params backward(Eigen::MatrixXd input);
+  void zero_grad_params();
 
  protected:
   inline double sample();
@@ -53,16 +56,25 @@ class LanguageModel {
   std::vector<double> input_hidden_b_buf_;
   std::vector<double> hidden_output_w_buf_;
   std::vector<double> hidden_output_b_buf_;
-  std::vector<Vector_t> wordvec_w_;
-  std::vector<Matrix_t> input_hidden_w_;
-  std::unique_ptr<Vector_t> input_hidden_b_;
-  std::unique_ptr<Matrix_t> hidden_output_w_;
-  std::unique_ptr<Vector_t> hidden_output_b_;
+  std::vector<Map<Vector_t>> wordvec_w_;
+  std::vector<Map<Matrix_t>> input_hidden_w_;
+  std::unique_ptr<Map<Vector_t>> input_hidden_b_;
+  std::unique_ptr<Map<Matrix_t>> hidden_output_w_;
+  std::unique_ptr<Map<Vector_t>> hidden_output_b_;
 
   // Cached activations
-  Eigen::VectorXd hidden_, hidden_tanh_;
-  Eigen::VectorXd output_, output_normed_;
+  Vector_t hidden_, hidden_tanh_;
+  Vector_t output_, output_normed_;
+
+  // Log partition function
   double logZ_;
+
+  // Gradients
+  std::unordered_map<uint32_t, Vector_t> wordvec_w_grad_;
+  std::vector<Matrix_t> input_hidden_w_grad_;
+  Vector_t input_hidden_b_grad_;
+  Matrix_t hidden_output_w_grad_;
+  Vector_t hidden_output_b_grad_;
 };
 
 #endif
